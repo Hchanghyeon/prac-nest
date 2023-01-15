06cdca2b32ac3ae92163d3fdcd8beb4414833ce7
changhyeon <changhyeon.h@kakao.com>
2023-01-15 22:08:46 +0900

    fix: nodemailer pass

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -18,7 +18,7 @@ export class EmailService {
             service: 'Gmail',
             auth: {
                 user: 'changhyeonhw@gmail.com',
-                pass: 'gcujokpilbvcakst'
+                pass: 'test'
             }
         });
     }
