c1badbe37ab06862d5dd80c416fac2639d9736d9
changhyeon <changhyeon.h@kakao.com>
2023-01-16 23:15:32 +0900

    feat: Add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,24 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { EmailService } from './email/email.service';
+import { UsersModule } from './users/users.module';
+import { EmailModule } from './email/email.module';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService, EmailService],
+  imports: [UsersModule,
+    ConfigModule.forRoot({
+      envFilePath:[`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load:[emailConfig],
+      isGlobal:true,
+      validationSchema
+    })
+  ],
+  controllers: [],
+  providers:[],
 })
+
+
 export class AppModule {}
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,6 +1,8 @@
 import Mail = require('nodemailer/lib/mailer');
 import * as nodemailer from 'nodemailer';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import emailConfig from 'src/config/emailConfig';
+import { ConfigType } from '@nestjs/config';
 
 
 interface EmailOptions {
@@ -13,18 +15,18 @@ interface EmailOptions {
 export class EmailService {
     private transporter: Mail;
 
-    constructor(){
+    constructor(@Inject(emailConfig.KEY) private config: ConfigType<typeof emailConfig>){
         this.transporter = nodemailer.createTransport({
-            service: 'Gmail',
+            service: config.service,
             auth: {
-                user: 'changhyeonhw@gmail.com',
-                pass: 'test'
+                user: config.auth.user,
+                pass: config.auth.pass
             }
         });
     }
 
     async sendMemberJoinVerification(emailAddress: string, signupVerifyToken: string){
-        const baseUrl = 'http://localhost:3000';
+        const baseUrl = this.config.baseUrl;
 
         const url = `${baseUrl}/users/email-verify?signupVerifyToken=${signupVerifyToken}`;
 
